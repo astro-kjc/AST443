e1bb96fa7a9a8b999aac1711c8074785aafdf4e0
Kiran Eiden <kiran.eiden@stonybrook.edu>
2019-10-06 16:30:46 -0400

    New and improved lightcurve generator

--- Lab1/lightcurves.py
+++ Lab1/lightcurves.py
@@ -26,23 +26,42 @@ def is_kinda_in(a, b, eps):
         return (np.abs(b - el) < eps).any()
 
     return a.apply(map_one)
+    
+def distances(starcoords, catframe):
+    
+    alpha, delta = starcoords
+    dra = (alpha - catframe['Ra']) * np.cos(delta)
+    ddec = delta - catframe['Dec']
+    return np.sqrt(dra**2 + ddec**2)
+    
+def fuzzy_match(starcoords, catframe, eps=1e-3):
+    
+    gamma = distances(starcoords, catframe)
+    indx = gamma.idxmin()
+    
+    if gamma[indx] <= eps:
+        return catframe.loc[indx]
+    return None
+    
+def get_field(row_list, field):
+    
+    return [row[field] for row in row_list]
 
 kitties = glob.glob("data/*.cat")
 kitties = sorted(kitties)
 
-t = []
-time_series = []
+t = np.zeros(len(kitties), dtype=np.float64)
 NUMREF = 15
 reference_stars = None
-cat_less = []
-skipped = 0
+time_series = [list() for i in range(NUMREF)]
+indices = [list() for i in range(NUMREF)]
 
 for i, cat_name in enumerate(kitties):
 
     kitty = load_cat(cat_name)
     cat_num = cat_name.split('.')[-3]
     time = load_time(cat_num)
-
+    
     if reference_stars is None:
 
         kitty.sort_values(by='Flux', ascending=False, inplace=True)
@@ -54,53 +73,53 @@ for i, cat_name in enumerate(kitties):
 
         masked_kitty = kitty[mask][:NUMREF]
         masked_kitty.sort_values(by='Ra', inplace=True)
-        reference_stars = np.array([masked_kitty['Ra'], masked_kitty['Dec']])
+        reference_stars = np.array([masked_kitty['Ra'], masked_kitty['Dec']]).T
         
-    if int(cat_num) > 1403 and int(cat_num) < 1562:
-        epsilon = 7.5e-4
-    else:
-        epsilon = 5e-4
+    if len(kitty) <= 0: continue
         
-    kitty.sort_values(by='Ra', inplace=True)
-    mask = is_kinda_in(kitty['Ra'], reference_stars[0], epsilon)
-    mask &= is_kinda_in(kitty['Dec'], reference_stars[1], epsilon)
-    cut_kitty = kitty[mask]
-
-    if len(cut_kitty) == NUMREF:
-        time_series.append(cut_kitty)
-        t.append(time)
-    else:
-        skipped += 1
-        # print(cat_num, len(cut_kitty))
+    for j, starcoords in enumerate(reference_stars):
         
-print(f"Skipped: {100*skipped/len(kitties):.1f}%")
+        match = fuzzy_match(starcoords, kitty)
+        if match is not None:
+            time_series[j].append(match)
+            indices[j].append(i)
+            
+    t[i] = time
+    
+    prog = i / len(kitties)
+    numspace = int(50*(1-prog))
+    pbar = "[" + "#" * (50-numspace) + " " * numspace + "]"
+    end = "" if i < len(kitties) - 1 else "\n"
+    print(f"\r{pbar} {100*prog:.0f}%", end=end)
 
-flux = np.array([list(df['Flux']) for df in time_series])
-flux_err = np.array([list(df['Flux_err']) for df in time_series])
+flux = (get_field(row_list, "Flux") for row_list in time_series)
+flux = list(map(np.array, flux))
 
-mu = np.mean(flux, axis=0)
-flux /= mu
-flux_err /= mu
+flux_err = (get_field(row_list, "Flux_err") for row_list in time_series)
+flux_err = list(map(np.array, flux_err))
 
-flux = flux.T
-flux_err = flux_err.T
+mu = [flux_arr.mean() for flux_arr in flux]
+
+flux = [flux[i] / mu[i] for i in range(len(flux))]
+flux_err = [flux_err[i] / mu[i] for i in range(len(flux_err))]
 
 if not os.path.isdir("lightcurves"):
     os.mkdir("lightcurves")
 
+"""
 np.savetxt("lightcurves/ref_stars.csv", reference_stars, delimiter=",")
 np.savetxt("lightcurves/flux.csv", flux, delimiter=",")
 np.savetxt("lightcurves/flux_err.csv", flux_err, delimiter=",")
 np.savetxt("lightcurves/times.csv", t, delimiter=",")
+"""
 
 # I tried a bunch of subplots, but they weren't really readable
 
-for i in range(len(flux)):
+for i in range(NUMREF):
 
-    ra, dec = reference_stars[0][i], reference_stars[1][i]
-    plt.errorbar(t, flux[i], flux_err[i], fmt='.',
+    ra, dec = reference_stars[i]
+    plt.errorbar(t[indices[i]], flux[i], flux_err[i], fmt='.',
             label=rf"$\alpha$ = {ra:.2f}$^\circ$, $\delta$ = {dec:.2f}$^\circ$")
-    plt.ylim(min(flux[i].min(), 0.8), max(flux[i].max(), 1.2))
     plt.xlabel("Time Since Midnight UTC [s]")
     plt.ylabel(r"$\frac{f}{\langle f \rangle}$")
     plt.legend()
